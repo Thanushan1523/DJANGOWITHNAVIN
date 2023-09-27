d4d9e4b3575b883f67edcf732195ac9eac5aa56c
Thanushan1523 <thanushan1523@gmail.com>
2023-09-27 07:44:09 -0400

    initial commit

--- telusko/settings.py
+++ telusko/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l0_!_l+9kp_*u8frvzo9o&$cyb1_8brjz)pk0lc2trs%4dgl9+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
