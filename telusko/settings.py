3e6d3fac19a26be828343bdebcd73455482b6465
Thanushan1523 <thanushan1523@gmail.com>
2023-09-28 08:14:44 -0400

    veiws and templates

--- telusko/settings.py
+++ telusko/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l0_!_l+9kp_*u8frvzo9o&$cyb1_8brjz)pk0lc2trs%4dgl9+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'telusko.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates') ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
