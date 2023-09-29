125e2c572ebfa1ce9991c78b2eafce0b850a4dbf
Thanushan1523 <thanushan1523@gmail.com>
2023-09-29 07:35:29 -0400

    GET CHANGE INTO POST

--- calc/views.py
+++ calc/views.py
@@ -5,7 +5,8 @@ def home(request):
     return render(request, 'home.html', {'name':'thanushan'})
 
 def add(request):
-    val1=int(request.GET['num1'])
-    val2=int(request.GET['num2'])
+    val1=int(request.POST['num1'])
+    val2=int(request.POST['num2'])
     hi = val1 +val2
     return render (request,'result.html',{'result':hi})
+    
