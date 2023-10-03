14c22b8809eec2f25af641d0bb3ec69e133ebe9a
Thanushan1523 <thanushan1523@gmail.com>
2023-10-03 09:08:33 -0400

    static filesCPMPLETION WITHOUT HOMEPAGE DESIG

--- telusko/settings.py
+++ telusko/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS =[os.path.join(BASE_DIR,'static')]
-STATIC_URL = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
