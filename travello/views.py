8f7a6cafbba7cab45b20da8d8e4c9894cfa1b709
Thanushan1523 <thanushan1523@gmail.com>
2023-10-05 21:31:41 -0400

    passing dynamic data in html 1.1

--- travello/views.py
+++ travello/views.py
@@ -4,4 +4,21 @@ from .models import Destination
 # Create your views here.
 def index(request):
     dest1 = Destination()
-    return render(request, 'index.html',{'dest1' :dest1})
+    dest1.name= 'canada'
+    dest1.price=800
+    dest1.image='destination_1.jpg'
+
+    dest2 = Destination()
+    dest2.name= 'ontario'
+    dest2.price=1000
+    dest2.image='destination_2.jpg'
+
+    dest3 = Destination()
+    dest3.name= 'toronto'
+    dest3.price=2000
+    dest3.image='destination_1.jpg'
+
+    dests=[dest1,dest2,dest3]
+
+    
+    return render(request, 'index.html',{'dests' :dests})
